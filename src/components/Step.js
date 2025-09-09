d257deea2feb84f996de337847fac86d78a7669f
Sumitasawa <sumitasawa65@gmail.com>
2025-09-09 21:28:43 +0530

    Second commit

--- src/components/Step.js
+++ src/components/Step.js
@@ -3,81 +3,77 @@ import React from "react";
 const Step = ({ step, formData, errors, handleChange, nextStep, prevStep, handleSubmit }) => {
   return (
     <form onSubmit={handleSubmit}>
+      
       {step === 1 && (
-        <div>
+        <div id="step1">
           <h2>Customer Details</h2>
           <div>
             <label htmlFor="first_name">First Name:</label>
-            <br></br>
+            <br />
             <input
               type="text"
               id="first_name"
               value={formData.first_name}
               onChange={handleChange}
             />
-             <br></br>
           </div>
           <div>
             <label htmlFor="last_name">Last Name:</label>
-             <br></br>
+            <br />
             <input
               type="text"
               id="last_name"
               value={formData.last_name}
               onChange={handleChange}
             />
-             <br></br>
           </div>
         </div>
       )}
 
       {step === 2 && (
-        <div>
-          <h2>Step 2: Car Info</h2>
+        <div id="step2">
+          <h2>Car Info</h2>
           <div>
             <label htmlFor="model">Car Model:</label>
-             <br></br>
+            <br />
             <input
               type="text"
               id="model"
               value={formData.model}
               onChange={handleChange}
             />
-             <br></br>
           </div>
           <div>
             <label htmlFor="car_price">Car Price:</label>
-             <br></br>
+            <br />
             <input
               type="number"
               id="car_price"
               value={formData.car_price}
               onChange={handleChange}
             />
-             <br></br>
           </div>
         </div>
       )}
 
+   
       {step === 3 && (
-        <div>
+        <div id="step3">
           <h2>Payment Details</h2>
           <div>
             <label htmlFor="card_info">Credit Card Number:</label>
-             <br></br>
-            <br/>
+            <br />
             <input
               type="text"
               id="card_info"
               value={formData.card_info}
               onChange={handleChange}
             />
-             <br></br>
             {errors.card_info && <p style={{ color: "red" }}>{errors.card_info}</p>}
           </div>
           <div>
-            <label htmlFor="expiry_date">Expiry Date:</label>
-             <br></br>
+            <label htmlFor="expiry_date">Expiry Date (MM/YYYY):</label>
+            <br />
             <input
               type="text"
               id="expiry_date"
@@ -85,11 +81,9 @@ const Step = ({ step, formData, errors, handleChange, nextStep, prevStep, handle
               onChange={handleChange}
             />
             {errors.expiry_date && <p style={{ color: "red" }}>{errors.expiry_date}</p>}
-            
           </div>
         </div>
       )}
-
       <div className="navigation">
         {step > 1 && (
           <button type="button" onClick={prevStep}>
